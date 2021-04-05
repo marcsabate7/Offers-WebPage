7308650eeb0bf03261ce3ecd87ce223504d955cb
Marc <marc-saba@hotmail.com>
2021-04-05 21:26:18 +0200

    modified some issues with database and dockerfile/compose

--- offerscraping/settings.py
+++ offerscraping/settings.py
@@ -80,9 +80,10 @@ WSGI_APPLICATION = 'offerscraping.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'postgres',
         'USER': 'postgres',
+        'PASSWORD':'postgres',
         'HOST': 'db',
         'PORT': 5432,
     }
