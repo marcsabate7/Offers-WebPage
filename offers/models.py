083ea7309faa9538dc83b5bc676c69f8f33485db
Marc <marc-saba@hotmail.com>
2021-05-18 13:08:32 +0200

    models.py performed

--- offers/models.py
+++ offers/models.py
@@ -1,19 +1,26 @@
 from django.db import models
-from django.utils import timezone
+from django.urls import reverse
 from django.contrib.auth.models import User
+from django.utils import timezone
+from django.conf import settings
+from PIL import Image
 
 class Product(models.Model):
     id_product = models.AutoField(primary_key=True)
     product_name = models.CharField(max_length=100)
     category = models.CharField(max_length=100)
     description = models.TextField()
-    date_posted = models.DateTimeField(default=timezone.now)
-    #author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return u"%s" % self.product_name
 
 class Company(models.Model):
     company_name = models.CharField(max_length=100)
     city = models.CharField(max_length=100)
     num_workers = models.IntegerField()
+
+    def __str__(self):
+        return self.company_name
     
 class Maker(models.Model):
     company_name = models.CharField(max_length=100)
@@ -22,9 +29,26 @@ class Maker(models.Model):
 
 class Offer(models.Model):
     offer_id = models.AutoField(primary_key=True)
-    product_name = models.ForeignKey(Product, on_delete=models.CASCADE)
-    company_name = models.ForeignKey(Company, on_delete=models.CASCADE)
-    company_name = models.ForeignKey(Maker, on_delete=models.CASCADE)
+    product_name = models.ForeignKey(Product,on_delete=models.CASCADE)
+    company_name = models.CharField(max_length=100)
     new_price = models.IntegerField()
     old_price = models.IntegerField()
     discount = models.IntegerField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User,default=1,on_delete=models.CASCADE)
+    url_offer = models.URLField(max_length=250,default="",help_text="*In case the offer is local put the shop website URL")
+    address = models.CharField(max_length=1000,blank=True,help_text="*Add address in case the offer is in a local site")
+    image = models.ImageField(default='default.jpg',upload_to='offers')
+
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+        img = Image.open(self.image.path)
+        img.save(self.image.path)
+
+    def __str__(self):
+        return u"%s" % self.product_name
+
+    def get_absolute_url(self):
+        return reverse('offer-detail',kwargs={'pk':self.pk})
