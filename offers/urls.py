fab08292709823aecd617fba40c87a54a0f777a2
Marc <marc-saba@hotmail.com>
2021-05-18 13:10:30 +0200

    corresponding urls added to offers in order to detail offers / create offers, products / delete and update

--- offers/urls.py
+++ offers/urls.py
@@ -1,9 +1,23 @@
 from django.urls import path
-from .views import OfferListView,OfferDetailView,OfferCreateView
+from .views import OfferListView,OfferDetailView,OfferUpdateView,OfferDeleteView
+from .views import OfferCreateView
+from offers import views as offers_views
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+
 
 urlpatterns = [
     path('', OfferListView.as_view(),name='offers-page'),
+    path('offer/new/',OfferCreateView.as_view(),name='offer-create'),
     path('offer/<int:pk>/', OfferDetailView.as_view(),name='offer-detail'),
-    path('offer/new/', OfferCreateView.as_view(),name='offer-create'),
+    path('offer/<int:pk>/update/', OfferUpdateView.as_view(),name='offer-update'),
+    path('offer/<int:pk>/delete/', OfferDeleteView.as_view(),name='offer-delete'),
+    path('product/new/', offers_views.createProduct,name='product-create'),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
