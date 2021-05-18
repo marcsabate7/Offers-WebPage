bbaae05a793d2f8f97eb08a667e6dbb2997d0c13
sergisabate <sergisabatevila@gmail.com>
2021-05-18 21:01:56 +0200

    updating steps

--- features/steps/list_offer.py
+++ features/steps/list_offer.py
@@ -2,9 +2,11 @@ from behave import *
 
 use_step_matcher("parse")
 
+
 @when('I list offers')
 def step_impl(context):
     context.browser.visit(context.get_url('offers:main'))
+
 
 @then('I\'m viewing a list containing')
 def step_impl(context):
@@ -12,7 +14,7 @@ def step_impl(context):
     for i, row in enumerate(context.table):
         assert row['name'] == offers_links[i].text
 
-@step('The list contains {count:n} Offers')
+
+@then('The list contains {count:n} Offers')
 def step_impl(context, count):
     assert count == len(context.browser.find_by_css('//html/body/div/article/div/h2/a'))
-
